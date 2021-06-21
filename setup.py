e313d455ca37bcb9c6f2bd35f5238b36883f8cb0
Owner <yourdatatalking@gmail.com>
2021-06-21 13:09:12 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='SubBasementTwo',
+	version='',
+	packages=[''],
+	url='',
+	license='',
+	author='owner',
+	author_email='',
+	description='', install_requires=['matplotlib', 'numpy']
+)
